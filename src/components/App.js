87b4a773f779798593cec6a3cc28e87898cc12f4
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2020-06-25 14:42:05 -0600

    Write placeholder routines (#22)
    Close #12

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,41 @@ import Field from "./Field";
 import Homebase from "./Homebase";
 import Counter from "./Counter";
 
+// eslint-disable-next-line
+function setStage(settings) {
+  // Number of monsters in stage (e.g., 50)
+  // Rate of monster creation (interval at which a new monster created; e.g., 3)
+  // Wave duration (interval at which rate accelerates; e.g., 10)
+  // Rate of acceleration (multiplier to apply to rate; e.g. 0.75)
+  // In the example above: 50 monsters need to be eliminated to move to next stage. A new monster is provided every 3 seconds. Every 10 seconds, the duration decreases by 25% -- instead of monsters being created every 3 seconds, new monsters get created every 2.25 seconds. After 10 more seconds, a new monster gets created every 1.6875 seconds
+  console.log("Setting stage");
+}
+
+// eslint-disable-next-line
+function addMonster() {
+  // Update state to add monster of randomColor to randomQueue of randomField
+  // Create new Monster component within the appropriate queue (this should be handled automatically)
+  console.log("Add a monster to a random queue");
+}
+
+// eslint-disable-next-line
+function strike() {
+  // Read hero coords and direction to determine which queue to strike
+  // Call Queue.strike() on appropriate queue
+  console.log(`Strike at the right queue!`);
+}
+
+// eslint-disable-next-line
+function endStreak() {
+  console.log("Streak is over!");
+}
+
+// eslint-disable-next-line
+function endStage() {
+  // Congratulate user, show score, then call App.setStage with settings for next level
+  console.log(`🏁 Congratulations! You completed the stage 🏁`);
+}
+
 function App() {
   return (
     <div className="App">
