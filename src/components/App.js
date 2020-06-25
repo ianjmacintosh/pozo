d737f14c4f9ab1474110b0d7cd0fc8ee4d2e105b
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2020-06-25 16:20:22 -0600

    Migrate movement functionality (#23)
    * Give App state
    
    This was a stateless component before, which I should probably read back
    up on.
    
    * Add mocked state based on design
    
    These values could all change (and have even as writing it out)
    
    * Invisibly handle state -- hero is still hidden
    
    * Apply styles to make hero update
    
    Uses a mix of CSS variables and classes. I'm not sure if this is
    confusing and weird, but I'm remembering:
    * First do a thing
    * Then do it better
    * Then do it well

--- src/components/App.js
+++ src/components/App.js
@@ -6,59 +6,151 @@ import Field from "./Field";
 import Homebase from "./Homebase";
 import Counter from "./Counter";
 
-// eslint-disable-next-line
-function setStage(settings) {
-  // Number of monsters in stage (e.g., 50)
-  // Rate of monster creation (interval at which a new monster created; e.g., 3)
-  // Wave duration (interval at which rate accelerates; e.g., 10)
-  // Rate of acceleration (multiplier to apply to rate; e.g. 0.75)
-  // In the example above: 50 monsters need to be eliminated to move to next stage. A new monster is provided every 3 seconds. Every 10 seconds, the duration decreases by 25% -- instead of monsters being created every 3 seconds, new monsters get created every 2.25 seconds. After 10 more seconds, a new monster gets created every 1.6875 seconds
-  console.log("Setting stage");
-}
+class App extends React.Component {
+  state = {
+    board: {
+      north: {
+        // North and south queues will end the game when their length > 5
+        queueLengthLimit: 5,
+        queues: [
+          [
+            // Each item represents a monster where the value represents its color
+            0,
+          ],
+          [],
+          [],
+          [],
+        ],
+      },
+      west: {
+        // West and east queues will end the game when their length > 8
+        queueLengthLimit: 8,
+        queues: [[], [], [], []],
+      },
+      east: [[], [], [], []],
+      south: [[], [], [], []],
+      base: {
+        size: 4,
+      },
+    },
+    hero: {
+      color: 0,
+      x: 1,
+      y: 1,
+      orientation: "north",
+    },
+    streaking: true,
+    score: 200,
+    monstersRemaining: 28,
+  };
 
-// eslint-disable-next-line
-function addMonster() {
-  // Update state to add monster of randomColor to randomQueue of randomField
-  // Create new Monster component within the appropriate queue (this should be handled automatically)
-  console.log("Add a monster to a random queue");
-}
+  componentDidMount() {
+    window.addEventListener("keydown", this.walk);
+  }
 
-// eslint-disable-next-line
-function strike() {
-  // Read hero coords and direction to determine which queue to strike
-  // Call Queue.strike() on appropriate queue
-  console.log(`Strike at the right queue!`);
-}
+  setStage = (settings) => {
+    // Number of monsters in stage (e.g., 50)
+    // Rate of monster creation (interval at which a new monster created; e.g., 3)
+    // Wave duration (interval at which rate accelerates; e.g., 10)
+    // Rate of acceleration (multiplier to apply to rate; e.g. 0.75)
+    // In the example above: 50 monsters need to be eliminated to move to next stage. A new monster is provided every 3 seconds. Every 10 seconds, the duration decreases by 25% -- instead of monsters being created every 3 seconds, new monsters get created every 2.25 seconds. After 10 more seconds, a new monster gets created every 1.6875 seconds
+    console.log("Setting stage");
+  };
 
-// eslint-disable-next-line
-function endStreak() {
-  console.log("Streak is over!");
-}
+  addMonster = () => {
+    // Update state to add monster of randomColor to randomQueue of randomField
+    // Create new Monster component within the appropriate queue (this should be handled automatically)
+    console.log("Add a monster to a random queue");
+  };
 
-// eslint-disable-next-line
-function endStage() {
-  // Congratulate user, show score, then call App.setStage with settings for next level
-  console.log(`🏁 Congratulations! You completed the stage 🏁`);
-}
+  strike = () => {
+    // Read hero coords and direction to determine which queue to strike
+    // Call Queue.strike() on appropriate queue
+    console.log(`Strike at the right queue!`);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Scoreboard />
-      </header>
-      <main>
-        <Field className="north-field" />
-        <Field className="west-field" />
-        <Field className="east-field" />
-        <Field className="south-field" />
-        <Homebase />
-      </main>
-      <footer>
-        <Counter />
-      </footer>
-    </div>
-  );
+  endStreak = () => {
+    console.log("Streak is over!");
+  };
+
+  endStage = () => {
+    // Congratulate user, show score, then call App.setStage with settings for next level
+    console.log(`🏁 Congratulations! You completed the stage 🏁`);
+  };
+
+  // Walk accepts a direction, and calls move
+  walk = ({ key }) => {
+    // Each movement updates app state for hero x & y
+    const keyMappings = {
+      w: "up",
+      d: "right",
+      s: "down",
+      a: "left",
+
+      W: "up",
+      D: "right",
+      S: "down",
+      A: "left",
+
+      ArrowUp: "up",
+      ArrowRight: "right",
+      ArrowDown: "down",
+      ArrowLeft: "left",
+    };
+
+    if (key in keyMappings) {
+      const direction = keyMappings[key],
+        directionChanges = {
+          up: [0, -1],
+          right: [1, 0],
+          down: [0, 1],
+          left: [-1, 0],
+        },
+        baseSize = this.state.board.base.size;
+
+      // Update hero coordinates based on direction movement
+      let hero = { ...this.state.hero };
+
+      hero.direction = direction;
+
+      // Each coordinate must be between 1 and 4 (inclusive)
+      hero.x += directionChanges[direction][0];
+      hero.x = Math.max(1, hero.x);
+      hero.x = Math.min(baseSize, hero.x);
+
+      hero.y += directionChanges[direction][1];
+      hero.y = Math.max(1, hero.y);
+      hero.y = Math.min(baseSize, hero.y);
+
+      this.setState({ hero });
+
+      console.log(this.state.hero);
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Scoreboard />
+        </header>
+        <main>
+          <Field className="north-field" />
+          <Field className="west-field" />
+          <Field className="east-field" />
+          <Field className="south-field" />
+          <Homebase
+            heroX={this.state.hero.x}
+            heroY={this.state.hero.y}
+            heroDirection={this.state.hero.direction}
+          />
+        </main>
+        <footer>
+          <Counter />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,28 @@
 import React from "react";
 
 class Hero extends React.Component {
-  // Walk accepts a direction, and calls move
-  walk = (direction) => {
-    // Each movement updates app state for hero x & y
-    // CSS variables get updated to move Hero within grid (grid-row & grid-column to reflect x and y)
-    console.log("Go " + direction);
-  };
-
   changeColor = (newColor) => {
     // Update app state for hero color
     console.log(`Now I'm ${newColor}`);
   };
 
   render() {
-    return <div>Hero goes here</div>;
+    return (
+      <div
+        className={`hero hero--${this.props.direction}`}
+        style={{
+          "--hero-x": this.props.x,
+          "--hero-y": this.props.y,
+        }}
+      >
+        <svg>
+          <path
+            xmlns="http://www.w3.org/2000/svg"
+            d="m5,35l14,-30l14,30l-27,0z"
+          />
+        </svg>
+      </div>
+    );
   }
 }
 
