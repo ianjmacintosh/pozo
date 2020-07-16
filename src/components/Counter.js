739d60c67c104846af0fc817a5b680042081d196
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-07-16 14:38:44 -0600

    Fix Counter attribute typo

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Counter extends React.Component {
   render() {
-    return <div class="counter">{this.props.count}</div>;
+    return <div className="counter">{this.props.count}</div>;
   }
 }
 
