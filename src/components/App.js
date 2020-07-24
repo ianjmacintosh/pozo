c0f80a14320dfa1c80a66fa95a1713b76cef840f
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-07-24 14:04:08 -0600

    Remove setStage routine
    The functionality called in this routine is handled by the `start()`
    routine, `setStage()` is never actually called

--- src/components/App.js
+++ src/components/App.js
@@ -211,19 +211,6 @@ class App extends React.Component {
     }
   };
 
-  setStage = (stageNumber) => {
-    const stageSettings = stages[stageNumber];
-
-    // Number of monsters in stage (e.g., 50)
-    this.setState({ stageSettings, alertText: `Stage ${stageNumber}` });
-    console.log("New stage beginning");
-
-    // Rate of monster creation (interval at which a new monster created; e.g., 3)
-    // Wave duration (interval at which rate accelerates; e.g., 10)
-    // Rate of acceleration (multiplier to apply to rate; e.g. 0.75)
-    // In the example above: 50 monsters need to be eliminated to move to next stage. A new monster is provided every 3 seconds. Every 10 seconds, the duration decreases by 25% -- instead of monsters being created every 3 seconds, new monsters get created every 2.25 seconds. After 10 more seconds, a new monster gets created every 1.6875 seconds
-  };
-
   monsterTimer = null;
   waveTimer = null;
 
