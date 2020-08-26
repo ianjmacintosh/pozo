08a9491b57406cd22dc05342af4171f613a210c2
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-08-25 18:16:51 -0600

    Update shapes and syntax

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -17,13 +17,16 @@ class Monster extends React.Component {
               x="-1"
             />
           </g>
-          <g class="squiggle shape">
+          <g class="square shape">
             <title>Layer 1</title>
-            <path
-              transform="rotate(38 19,19)"
-              d="m29.92704,25.14157c-2.85768,0 -4.42064,-2.81353 -5.93171,-5.53435c-1.20202,-2.16341 -2.44487,-4.39966 -4.0023,-4.39966c-1.55854,0 -2.80029,2.23626 -4.0023,4.39966c-1.51218,2.72082 -3.07513,5.53435 -5.93171,5.53435c-2.92501,0 -4.25286,-2.98683 -5.42397,-5.62265c-1.15676,-2.60492 -2.01992,-4.31136 -3.40626,-4.31136c-0.60929,0 -1.10378,-0.49449 -1.10378,-1.10378s0.49449,-1.10378 1.10378,-1.10378c2.92502,0 4.25286,2.98683 5.42397,5.62265c1.15676,2.60382 2.01992,4.31136 3.40626,4.31136c1.55854,0 2.80029,-2.23626 4.0023,-4.39966c1.51218,-2.72081 3.07513,-5.53435 5.93171,-5.53435c2.85768,0 4.42064,2.81353 5.93171,5.53435c1.20202,2.16341 2.44487,4.39966 4.0023,4.39966c1.38635,0 2.2484,-1.70755 3.40626,-4.31136c1.17111,-2.63582 2.50006,-5.62265 5.42397,-5.62265c0.61039,0 1.10378,0.49449 1.10378,1.10378s-0.49339,1.10378 -1.10378,1.10378c-1.38635,0 -2.2484,1.70644 -3.40626,4.31136c-1.17111,2.63582 -2.50006,5.62265 -5.42397,5.62265z"
-              fill="#f00"
-              stroke="null"
+            <rect
+              transform="rotate(15 19.999999999999986,19.953125000000007) "
+              id="svg_1"
+              height="29"
+              width="29"
+              y="5.45313"
+              x="5.5"
+              fill="#000000"
             />
           </g>
           <g class="half-circle shape">
@@ -34,9 +37,9 @@ class Monster extends React.Component {
               width="0"
               y="-6.87445"
               x="-25.93655"
-              fill-opacity="null"
-              stroke-opacity="null"
-              stroke-width="null"
+              fillOpacity="null"
+              strokeOpacity="null"
+              strokeWidth="null"
               stroke="null"
               fill="#ff5400"
             />
@@ -44,11 +47,11 @@ class Monster extends React.Component {
               id="svg_9"
               d="m0.31291,20.0625c0,-10.87685 8.81024,-19.6871 19.68709,-19.6871c10.87685,0 19.68709,8.81025 19.68709,19.6871c0,10.87685 -8.81024,19.68709 -19.68709,19.68709l-19.68709,-19.68709z"
               stroke-opacity="null"
-              stroke-linecap="null"
-              stroke-linejoin="null"
-              stroke-width="null"
+              strokeLinecap="null"
+              strokeLinejoin="null"
+              strokeWidth="null"
               stroke="null"
-              fill-opacity="null"
+              fillOpacity="null"
               fill="#ff5400"
             />
           </g>
@@ -57,9 +60,9 @@ class Monster extends React.Component {
               transform="rotate(40 24.790212631225597,15.353872299194334) "
               id="svg_16"
               d="m8.66554,29.46296l16.12467,-28.21817l16.12467,28.21817l-32.24934,0z"
-              fill-opacity="null"
-              stroke-opacity="null"
-              stroke-width="null"
+              fillOpacity="null"
+              strokeOpacity="null"
+              strokeWidth="null"
               stroke="null"
               fill="#ff5400"
             />
