7b75d296c3a0efb1f84bfc9f01f5ec0e35efb48c
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2020-08-25 18:28:08 -0600

    Add shapes (#115)
    * Support different shapes for monsters
    
    These monster shapes are really not good, I will probably need to
    ask for a favor to get good ones
    
    * Update shapes and syntax
    
    * Fix additional syntax goofs
    
    * Add plus shape

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -17,14 +17,89 @@ class Monster extends React.Component {
               x="-1"
             />
           </g>
-          <g>
+          <g className="square shape">
             <title>Layer 1</title>
-            <path
+            <rect
+              transform="rotate(15 19.999999999999986,19.953125000000007) "
               id="svg_1"
-              d="m15.854001,20.518823c3.583026,0.859926 5.446199,7.452693 11.752324,9.029224c6.306125,1.576531 12.468929,-10.175793 6.306125,-14.762065c-6.162804,-4.586273 -9.172545,6.892844 -14.403763,3.23144c-5.231217,-3.661404 -5.231217,-12.103909 -11.895645,-8.742582c-6.664428,3.361327 -6.503191,10.403651 -1.316762,12.113706c5.186429,1.710055 5.974695,-1.72965 9.557721,-0.869724l0,0.000001z"
-              fill="#f00"
-              stroke="null"
+              height="29"
+              width="29"
+              y="5.45313"
+              x="5.5"
+              fill="#000000"
             />
+          </g>
+          <g className="half-circle shape">
+            <title>Layer 1</title>
+            <rect
+              id="svg_6"
+              height="0.125"
+              width="0"
+              y="-6.87445"
+              x="-25.93655"
+              fillOpacity="null"
+              strokeOpacity="null"
+              strokeWidth="null"
+              stroke="null"
+              fill="#ff5400"
+            />
+            <path
+              id="svg_9"
+              d="m0.31291,20.0625c0,-10.87685 8.81024,-19.6871 19.68709,-19.6871c10.87685,0 19.68709,8.81025 19.68709,19.6871c0,10.87685 -8.81024,19.68709 -19.68709,19.68709l-19.68709,-19.68709z"
+              strokeOpacity="null"
+              strokeLinecap="null"
+              strokeLinejoin="null"
+              strokeWidth="null"
+              stroke="null"
+              fillOpacity="null"
+              fill="#ff5400"
+            />
+          </g>
+          <g className="triangle shape">
+            <path
+              transform="rotate(40 24.790212631225597,15.353872299194334) "
+              id="svg_16"
+              d="m8.66554,29.46296l16.12467,-28.21817l16.12467,28.21817l-32.24934,0z"
+              fillOpacity="null"
+              strokeOpacity="null"
+              strokeWidth="null"
+              stroke="null"
+              fill="#ff5400"
+            />
+          </g>
+          <g className="plus shape">
+            <g>
+              <g
+                display="none"
+                overflow="visible"
+                y="0"
+                x="0"
+                height="100%"
+                width="100%"
+                id="canvasGrid"
+              >
+                <rect
+                  fill="url(#gridpattern)"
+                  strokeWidth="0"
+                  y="0"
+                  x="0"
+                  height="100%"
+                  width="100%"
+                />
+              </g>
+            </g>
+            <g>
+              <path
+                transform="rotate(-98 20.000890731811523,20.000890731811527) "
+                stroke="#0f0f00"
+                id="svg_3"
+                d="m1.00089,10.21395l9.21306,-9.21306l9.78689,9.78681l9.78689,-9.78681l9.21315,9.21306l-9.7869,9.78689l9.7869,9.78689l-9.21315,9.21315l-9.78689,-9.7869l-9.78689,9.7869l-9.21306,-9.21315l9.78681,-9.78689l-9.78681,-9.78689z"
+                fillOpacity="null"
+                strokeOpacity="null"
+                strokeWidth="0"
+                fill="#000000"
+              />
+            </g>
           </g>
         </svg>
       </li>
