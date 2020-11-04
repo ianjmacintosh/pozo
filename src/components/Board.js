038f7dbdf2bc73ff4dffdaeecf90d3dc076a8241
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-11-04 10:35:29 -0300

    Group methods by ownership

--- src/components/Board.js
+++ src/components/Board.js
@@ -129,116 +129,6 @@ class Board extends React.Component {
     window.addEventListener("keydown", this.handleKeypress);
   }
 
-  // Homebase needs this
-  changeColor = (newColor) => {
-    const hero = { ...this.state.hero };
-    hero.color = newColor;
-
-    // Update app state for hero color
-    this.setState({ hero });
-  };
-
-  // Scoreboard needs this
-  reportElimination = (monstersEliminated) => {
-    this.props.playSound("eliminate");
-    this.updateScoreboard(monstersEliminated);
-    this.updateCounter(monstersEliminated);
-  };
-
-  // Scoreboard needs this (probably)
-  endStreak = () => {
-    this.setState({ streak: 0 });
-  };
-
-  // Multiple components need this; Hero and Field
-  pause = () => {
-    let paused = this.state.paused;
-
-    if (paused) {
-      this.props.showAlert(
-        <React.Fragment>
-          <h1>Go!</h1>
-        </React.Fragment>
-      );
-
-      // Resume the timers
-      this.monsterTimer = window.setInterval(
-        this.chooseQueue,
-        this.state.stageSettings.creationRate * 1000
-      );
-
-      this.waveTimer = window.setInterval(() => {
-        let stageSettings = this.state.stageSettings;
-        stageSettings.creationRate =
-          stageSettings.creationRate / stageSettings.rateMultiplier;
-        clearInterval(this.monsterTimer);
-
-        this.monsterTimer = window.setInterval(
-          this.chooseQueue,
-          this.state.stageSettings.creationRate * 1000
-        );
-      }, this.state.stageSettings.waveDuration * 1000);
-    } else {
-      // Save the time remaining on monsterTimer
-      this.props.showAlert(
-        <React.Fragment>
-          <h1>Paused</h1>
-        </React.Fragment>
-      );
-      // Clear the timers
-      clearInterval(this.monsterTimer);
-      clearInterval(this.waveTimer);
-    }
-
-    paused = !paused;
-    this.setState({ paused });
-  };
-
-  // Homebase needs this
-  squareSize = () => {
-    return parseInt(
-      getComputedStyle(document.querySelector(".board")).getPropertyValue(
-        "--square-size"
-      ),
-      10
-    );
-  };
-
-  // Multiple components need this; Hero, Field, and Alert
-  endStage = (playerDidWin) => {
-    clearInterval(this.monsterTimer);
-    clearInterval(this.waveTimer);
-    this.setState({ redAlert: false });
-
-    if (playerDidWin) {
-      let currentStage = this.state.currentStage + 1;
-      if (stages[currentStage]) {
-        this.props.playSound("stageClear");
-        this.setState({ currentStage });
-        this.start(currentStage);
-      } else {
-        this.props.changeGameActive(false);
-        this.props.showAlert("victory", false);
-      }
-    } else {
-      this.props.changeGameActive(false);
-      this.props.playSound("gameOver");
-      this.props.showAlert("gameOver", false);
-    }
-  };
-
-  // Scoreboard needs this
-  updateScoreboard = (monsterCount) => {
-    let score = this.state.score,
-      streak = this.state.streak,
-      pointsToAdd = monsterCount * 100 * streak;
-
-    score += pointsToAdd;
-
-    // Scoreboard.update() manages streak tally? Need to figure out how to manage this
-    this.setState({ score });
-  };
-
   // Counter needs this
   updateCounter = (monsterCount) => {
     let monstersRemaining = this.state.monstersRemaining;
@@ -254,180 +144,53 @@ class Board extends React.Component {
     });
   };
 
-  // Multiple components need this; Hero and whoever needs "pause"
-  handleKeypress = ({ key }) => {
+  // Homebase needs this
+  changeColor = (newColor) => {
+    const hero = { ...this.state.hero };
+    hero.color = newColor;
+
+    // Update app state for hero color
+    this.setState({ hero });
+  };
+
+  // Homebase needs this
+  // Walk accepts a direction, and calls move
+  walk = (direction) => {
+    this.props.playSound("walk", 0, 0.15);
     // Each movement updates app state for hero x & y
-    const keyMappings = {
-      Escape: "pause",
+    const directionChanges = {
+        up: [0, -1],
+        right: [1, 0],
+        down: [0, 1],
+        left: [-1, 0],
+      },
+      baseSize = this.state.base.size;
 
-      w: "up",
-      d: "right",
-      s: "down",
-      a: "left",
-      " ": "strike",
-      z: "strike",
+    // Update hero coordinates based on direction movement
+    let hero = { ...this.state.hero };
 
-      W: "up",
-      D: "right",
-      S: "down",
-      A: "left",
-      Z: "strike",
+    hero.orientation = direction;
 
-      ArrowUp: "up",
-      ArrowRight: "right",
-      ArrowDown: "down",
-      ArrowLeft: "left",
-      Enter: "strike",
-    };
+    // Each coordinate must be between 1 and 4 (inclusive)
+    hero.x += directionChanges[direction][0];
+    hero.x = Math.max(1, hero.x);
+    hero.x = Math.min(baseSize, hero.x);
 
-    if (key in keyMappings) {
-      const command = keyMappings[key];
-      // If it's a pause button, run the pause command
-      if (command === "pause") {
-        this.pause();
-        return;
-      }
+    hero.y += directionChanges[direction][1];
+    hero.y = Math.max(1, hero.y);
+    hero.y = Math.min(baseSize, hero.y);
 
-      const isPaused = this.state.paused;
-      if (isPaused) {
-        return;
-      }
-
-      // Determine if user is controlling hero in game or navigating menu
-      // If playing game:
-      if (this.props.isGameActive) {
-        if (keyMappings[key] === "strike") {
-          let direction = this.state.hero.orientation,
-            queue = this.state.hero.y - 1,
-            color = this.state.hero.color;
-
-          // If the hero is pointed north or south, use X coord for queue
-          if (direction === "up" || direction === "down") {
-            queue = this.state.hero.x - 1;
-          }
-
-          this.strike(direction, queue, color);
-        } else {
-          this.walk(keyMappings[key]);
-        }
-      }
-    }
+    this.setState({ hero });
   };
 
-  // Field needs this
-  // Update state to add monster of randomColor to randomQueue of randomField
-  addMonster = (direction = "up", queueNumber = 0, colorNumber = 0) => {
-    // Update the state for the given queue to add a monster to it
-    // Make a copy of the field
-    let fields = this.state.fields;
-
-    // Add a monster to the front of it
-    fields[direction].queues[queueNumber].push({
-      type: "monster",
-      color: colorNumber,
-    });
-
-    // Update the state
-    this.setState({ fields });
-
-    // Determine if we're over the max queue length and if so, end the stage
-    if (
-      fields[direction].queues[queueNumber].length >
-      fields[direction].queueLengthLimit
-    ) {
-      this.endStage();
-    }
-
-    // If there's 1 square or fewer between the home base and the monster, go red alert
-    if (
-      fields[direction].queueLengthLimit -
-        fields[direction].queues[queueNumber].length <=
-      1
-    ) {
-      this.setState({ redAlert: true });
-    }
-
-    // Create new Monster component within the appropriate queue (this should be handled automatically)
-  };
-
-  // Field needs this
-  chooseQueue = () => {
-    console.log("Choosing a queue");
-    let fieldNumber = getRandomInt(0, 3),
-      queueNumber = getRandomInt(0, 3),
-      colorNumber = getRandomInt(0, 3);
-
-    // If queue length is 2 monsters more than any other in field:
-    // Get length of this queue
-    let field = this.state.fields[directionMap[fieldNumber]];
-
-    let fieldWouldBeUnbalanced = (allQueues, targetQueue) =>
-      field.queues.some(
-        (thisQueue) => allQueues[targetQueue].length - thisQueue.length > 1
-      );
-
-    // Get length of shortest queue
-    while (fieldWouldBeUnbalanced(field.queues, queueNumber)) {
-      queueNumber = getRandomInt(0, 3);
-    }
-    // If longest queue - shortest queue is > 2, choose a different queue
-    // queueNumber = getRandomInt(0, 3);
-    this.addMonster(directionMap[fieldNumber], queueNumber, colorNumber);
-  };
-
-  // Multiple components need this; Field and Hero
-  start = (stageNumber = 0) => {
-    // Activate game
-    this.setState({ gameActive: true, redAlert: false }, () => {
-      if (stageNumber === 0) {
-        this.props.playSound("menuSelect", 0, 0.2);
-      }
-    });
-
-    // Clear all queues
-    let fields = this.state.fields;
-    fields.up.queues = [[], [], [], []];
-    fields.down.queues = [[], [], [], []];
-    fields.left.queues = [[], [], [], []];
-    fields.right.queues = [[], [], [], []];
-    this.setState({ fields });
-
-    const stageSettings = { ...stages[stageNumber] },
-      setTimers = () => {
-        clearInterval(this.monsterTimer);
-        clearInterval(this.waveTimer);
-
-        this.monsterTimer = window.setInterval(
-          this.chooseQueue,
-          this.state.stageSettings.creationRate * 1000
-        );
-
-        this.waveTimer = window.setInterval(() => {
-          let stageSettings = this.state.stageSettings;
-          stageSettings.creationRate =
-            stageSettings.creationRate / stageSettings.rateMultiplier;
-          clearInterval(this.monsterTimer);
-
-          this.monsterTimer = window.setInterval(
-            this.chooseQueue,
-            this.state.stageSettings.creationRate * 1000
-          );
-        }, this.state.stageSettings.waveDuration * 1000);
-      };
-
-    // Apply stage color scheme
-    document.body.classList.remove(`stage${stageNumber - 1}`);
-    document.body.classList.add(`stage${stageNumber}`);
-
-    // Number of monsters in stage (e.g., 50)
-    this.setState({ stageSettings }, setTimers);
-    this.setState({
-      monstersRemaining: stageSettings.monsters,
-    });
-    this.props.updateAlert("stageAnnouncement", {
-      content: <h1>{`Stage ${this.state.currentStage}`}</h1>,
-    });
-    this.props.showAlert("stageAnnouncement");
+  // Homebase needs this
+  squareSize = () => {
+    return parseInt(
+      getComputedStyle(document.querySelector(".board")).getPropertyValue(
+        "--square-size"
+      ),
+      10
+    );
   };
 
   // Homebase needs this
@@ -592,34 +355,271 @@ class Board extends React.Component {
     this.setState({ hero });
   };
 
-  // Homebase needs this
-  // Walk accepts a direction, and calls move
-  walk = (direction) => {
-    this.props.playSound("walk", 0, 0.15);
+  // Scoreboard needs this
+  reportElimination = (monstersEliminated) => {
+    this.props.playSound("eliminate");
+    this.updateScoreboard(monstersEliminated);
+    this.updateCounter(monstersEliminated);
+  };
+
+  // Scoreboard needs this
+  updateScoreboard = (monsterCount) => {
+    let score = this.state.score,
+      streak = this.state.streak,
+      pointsToAdd = monsterCount * 100 * streak;
+
+    score += pointsToAdd;
+
+    // Scoreboard.update() manages streak tally? Need to figure out how to manage this
+    this.setState({ score });
+  };
+
+  // Scoreboard needs this (probably)
+  endStreak = () => {
+    this.setState({ streak: 0 });
+  };
+
+  // Multiple components need this; Hero and Field
+  pause = () => {
+    let paused = this.state.paused;
+
+    if (paused) {
+      this.props.showAlert(
+        <React.Fragment>
+          <h1>Go!</h1>
+        </React.Fragment>
+      );
+
+      // Resume the timers
+      this.monsterTimer = window.setInterval(
+        this.chooseQueue,
+        this.state.stageSettings.creationRate * 1000
+      );
+
+      this.waveTimer = window.setInterval(() => {
+        let stageSettings = this.state.stageSettings;
+        stageSettings.creationRate =
+          stageSettings.creationRate / stageSettings.rateMultiplier;
+        clearInterval(this.monsterTimer);
+
+        this.monsterTimer = window.setInterval(
+          this.chooseQueue,
+          this.state.stageSettings.creationRate * 1000
+        );
+      }, this.state.stageSettings.waveDuration * 1000);
+    } else {
+      // Save the time remaining on monsterTimer
+      this.props.showAlert(
+        <React.Fragment>
+          <h1>Paused</h1>
+        </React.Fragment>
+      );
+      // Clear the timers
+      clearInterval(this.monsterTimer);
+      clearInterval(this.waveTimer);
+    }
+
+    paused = !paused;
+    this.setState({ paused });
+  };
+
+  // Field needs this
+  // Update state to add monster of randomColor to randomQueue of randomField
+  addMonster = (direction = "up", queueNumber = 0, colorNumber = 0) => {
+    // Update the state for the given queue to add a monster to it
+    // Make a copy of the field
+    let fields = this.state.fields;
+
+    // Add a monster to the front of it
+    fields[direction].queues[queueNumber].push({
+      type: "monster",
+      color: colorNumber,
+    });
+
+    // Update the state
+    this.setState({ fields });
+
+    // Determine if we're over the max queue length and if so, end the stage
+    if (
+      fields[direction].queues[queueNumber].length >
+      fields[direction].queueLengthLimit
+    ) {
+      this.endStage();
+    }
+
+    // If there's 1 square or fewer between the home base and the monster, go red alert
+    if (
+      fields[direction].queueLengthLimit -
+        fields[direction].queues[queueNumber].length <=
+      1
+    ) {
+      this.setState({ redAlert: true });
+    }
+
+    // Create new Monster component within the appropriate queue (this should be handled automatically)
+  };
+
+  // Field needs this
+  chooseQueue = () => {
+    console.log("Choosing a queue");
+    let fieldNumber = getRandomInt(0, 3),
+      queueNumber = getRandomInt(0, 3),
+      colorNumber = getRandomInt(0, 3);
+
+    // If queue length is 2 monsters more than any other in field:
+    // Get length of this queue
+    let field = this.state.fields[directionMap[fieldNumber]];
+
+    let fieldWouldBeUnbalanced = (allQueues, targetQueue) =>
+      field.queues.some(
+        (thisQueue) => allQueues[targetQueue].length - thisQueue.length > 1
+      );
+
+    // Get length of shortest queue
+    while (fieldWouldBeUnbalanced(field.queues, queueNumber)) {
+      queueNumber = getRandomInt(0, 3);
+    }
+    // If longest queue - shortest queue is > 2, choose a different queue
+    // queueNumber = getRandomInt(0, 3);
+    this.addMonster(directionMap[fieldNumber], queueNumber, colorNumber);
+  };
+
+  // Multiple components need this; Hero, Field, and Alert
+  endStage = (playerDidWin) => {
+    clearInterval(this.monsterTimer);
+    clearInterval(this.waveTimer);
+    this.setState({ redAlert: false });
+
+    if (playerDidWin) {
+      let currentStage = this.state.currentStage + 1;
+      if (stages[currentStage]) {
+        this.props.playSound("stageClear");
+        this.setState({ currentStage });
+        this.start(currentStage);
+      } else {
+        this.props.changeGameActive(false);
+        this.props.showAlert("victory", false);
+      }
+    } else {
+      this.props.changeGameActive(false);
+      this.props.playSound("gameOver");
+      this.props.showAlert("gameOver", false);
+    }
+  };
+
+  // Multiple components need this; Hero and whoever needs "pause"
+  handleKeypress = ({ key }) => {
     // Each movement updates app state for hero x & y
-    const directionChanges = {
-        up: [0, -1],
-        right: [1, 0],
-        down: [0, 1],
-        left: [-1, 0],
-      },
-      baseSize = this.state.base.size;
+    const keyMappings = {
+      Escape: "pause",
 
-    // Update hero coordinates based on direction movement
-    let hero = { ...this.state.hero };
+      w: "up",
+      d: "right",
+      s: "down",
+      a: "left",
+      " ": "strike",
+      z: "strike",
 
-    hero.orientation = direction;
+      W: "up",
+      D: "right",
+      S: "down",
+      A: "left",
+      Z: "strike",
 
-    // Each coordinate must be between 1 and 4 (inclusive)
-    hero.x += directionChanges[direction][0];
-    hero.x = Math.max(1, hero.x);
-    hero.x = Math.min(baseSize, hero.x);
+      ArrowUp: "up",
+      ArrowRight: "right",
+      ArrowDown: "down",
+      ArrowLeft: "left",
+      Enter: "strike",
+    };
 
-    hero.y += directionChanges[direction][1];
-    hero.y = Math.max(1, hero.y);
-    hero.y = Math.min(baseSize, hero.y);
+    if (key in keyMappings) {
+      const command = keyMappings[key];
+      // If it's a pause button, run the pause command
+      if (command === "pause") {
+        this.pause();
+        return;
+      }
 
-    this.setState({ hero });
+      const isPaused = this.state.paused;
+      if (isPaused) {
+        return;
+      }
+
+      // Determine if user is controlling hero in game or navigating menu
+      // If playing game:
+      if (this.props.isGameActive) {
+        if (keyMappings[key] === "strike") {
+          let direction = this.state.hero.orientation,
+            queue = this.state.hero.y - 1,
+            color = this.state.hero.color;
+
+          // If the hero is pointed north or south, use X coord for queue
+          if (direction === "up" || direction === "down") {
+            queue = this.state.hero.x - 1;
+          }
+
+          this.strike(direction, queue, color);
+        } else {
+          this.walk(keyMappings[key]);
+        }
+      }
+    }
+  };
+
+  // Multiple components need this; Field and Hero
+  start = (stageNumber = 0) => {
+    // Activate game
+    this.setState({ gameActive: true, redAlert: false }, () => {
+      if (stageNumber === 0) {
+        this.props.playSound("menuSelect", 0, 0.2);
+      }
+    });
+
+    // Clear all queues
+    let fields = this.state.fields;
+    fields.up.queues = [[], [], [], []];
+    fields.down.queues = [[], [], [], []];
+    fields.left.queues = [[], [], [], []];
+    fields.right.queues = [[], [], [], []];
+    this.setState({ fields });
+
+    const stageSettings = { ...stages[stageNumber] },
+      setTimers = () => {
+        clearInterval(this.monsterTimer);
+        clearInterval(this.waveTimer);
+
+        this.monsterTimer = window.setInterval(
+          this.chooseQueue,
+          this.state.stageSettings.creationRate * 1000
+        );
+
+        this.waveTimer = window.setInterval(() => {
+          let stageSettings = this.state.stageSettings;
+          stageSettings.creationRate =
+            stageSettings.creationRate / stageSettings.rateMultiplier;
+          clearInterval(this.monsterTimer);
+
+          this.monsterTimer = window.setInterval(
+            this.chooseQueue,
+            this.state.stageSettings.creationRate * 1000
+          );
+        }, this.state.stageSettings.waveDuration * 1000);
+      };
+
+    // Apply stage color scheme
+    document.body.classList.remove(`stage${stageNumber - 1}`);
+    document.body.classList.add(`stage${stageNumber}`);
+
+    // Number of monsters in stage (e.g., 50)
+    this.setState({ stageSettings }, setTimers);
+    this.setState({
+      monstersRemaining: stageSettings.monsters,
+    });
+    this.props.updateAlert("stageAnnouncement", {
+      content: <h1>{`Stage ${this.state.currentStage}`}</h1>,
+    });
+    this.props.showAlert("stageAnnouncement");
   };
 
   render() {
