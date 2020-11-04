ced1d7c1ab8a3b4b308ed47c2658faa12f6d973c
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-11-04 14:25:20 -0300

    Move strike function into queue

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -5,12 +5,87 @@ import Ghost from "./Ghost";
 import "./Queue.css";
 
 class Queue extends React.Component {
-  reportElimination = () => {
-    // Set streak to on
-    // Call Counter.update()
-    // Call Monster.strike() on next monster (if exists)
-    // If no next monster exists on queue, call Scoreboard.update()
-  };
+  state = {
+    contents: []
+  }
+
+  strike = (color) => {
+    console.log(`Struck with ${color}`);
+
+    let contents = this.state.contents,
+      monsterQueue = contents.filter((item) => item.type === "monster"),
+      monsterColor,
+      topMonster;
+
+    if (monsterQueue.length > 0) {
+      topMonster = contents.find((item) => item.type === "monster");
+      monsterColor = topMonster.color;
+    } else {
+      monsterColor = null;
+    }
+
+    // If monster is same color, eliminate it
+    if (color === monsterColor) {
+      // Update streak
+      const streak = 1 + this.state.streak;
+      this.setState({ streak });
+
+      // Convert monster to ghost
+      topMonster.content = 100 * streak;
+      topMonster.type = "ghost";
+
+      // Remove the ghost
+      setTimeout(() => {
+        const index = contents.indexOf(topMonster);
+        contents.splice(index, 1);
+      }, 2000);
+
+      // // Measure queues length to update "red alert" status
+      // const hasEnoughRoom = (queue, lengthLimit) => {
+      //   const nonGhosts = queue.filter((element) => element.type !== "ghost");
+
+      //   return lengthLimit - nonGhosts.length > 1;
+      // };
+
+      // if (
+      //   fields.down.queues.every((queue) =>
+      //     hasEnoughRoom(queue, fields.down.queueLengthLimit)
+      //   ) &&
+      //   fields.up.queues.every((queue) =>
+      //     hasEnoughRoom(queue, fields.up.queueLengthLimit)
+      //   ) &&
+      //   fields.left.queues.every((queue) =>
+      //     hasEnoughRoom(queue, fields.left.queueLengthLimit)
+      //   ) &&
+      //   fields.right.queues.every((queue) =>
+      //     hasEnoughRoom(queue, fields.right.queueLengthLimit)
+      //   )
+      // ) {
+      //   this.setState({ redAlert: false });
+      // }
+
+      this.props.reportElimination(1);
+      this.setState({ contents });
+      this.strike(color);
+      return;
+    }
+    // If there's a monster in the queue struck, swap colors
+    else if (monsterQueue.length > 0) {
+      this.props.playSound("swap");
+      // Report streak end via App.endStreak()
+      if (this.state.streak > 0) {
+        this.endStreak();
+      }
+
+      //   Update hero color
+      this.changeColor(monsterColor);
+
+      //   Update monster color
+      monsterQueue[0].color = color;
+
+      this.setState({ contents });
+    }
+  }
 
   render() {
     return (
