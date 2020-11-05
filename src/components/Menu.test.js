cd355982c25435985c77ad0572526b439bb5fbd6
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-11-05 18:17:47 -0300

    Delete old test

--- src/components/Menu.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Menu from "./Menu";
-
-test("renders Pozo title", () => {
-  const { getByText } = render(<Menu />);
-  const bigTitleHeadline = getByText(/Pozo/i);
-  expect(bigTitleHeadline).toBeInTheDocument();
-});
