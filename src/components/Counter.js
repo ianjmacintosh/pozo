e974bafd1d3034978865682861ebdb20fd40c134
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-11-11 10:52:26 -0300

    Add animation for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Counter.css";
 
+import { getRandomInt } from "../helpers";
+
 class Counter extends React.Component {
+  state = {
+    animationName: ""
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.count !== this.props.count) {
+      if (!this.state.animating) {
+        this.setState({
+          animationName: "shake" + getRandomInt(1, 5)
+        })
+        setTimeout(() => {
+          this.setState({
+            animationName: ""
+          })
+        }, 500);
+      }
+    }
+  }
+
   render() {
     let monstersRemaining = this.props.count;
 
-    return <div className="counter">{monstersRemaining}</div>;
+    return <div className={`counter ${ this.state.animationName }`}>
+      {monstersRemaining}
+      </div>;
   }
 }
 
