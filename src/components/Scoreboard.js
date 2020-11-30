7de8d7a88843d800bd7c783ec05da09949216a3e
Ian MacIntosh <ianjmacintosh@gmail.com>
2020-11-30 15:37:41 -0300

    Update high score when score updates

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -6,13 +6,16 @@ class Scoreboard extends React.Component {
     highScore: 0
   };
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate() {
+    if (this.props.score > this.state.highScore) {
+      this.setState({ highScore: this.props.score })
+    }
   }
 
   render() {
     return <div className="scoreboard">
       <p className="current-score">
-      Score: {this.props.score}
+      Score: <span data-testid="current-score-display">{this.props.score}</span>
       </p>
       <p className="high-score">
         High Score: <span data-testid="high-score-display">{this.state.highScore}</span>
--- src/components/Scoreboard.test.js
+++ src/components/Scoreboard.test.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
+
 import Scoreboard from "./Scoreboard";
 
-let score = 0;
-render(<Scoreboard score={score}/>);
+test("updates the score when the prop updates", () => {
+    const { rerender } = render(<Scoreboard score={0}/>);
 
-const highScore = screen.getByTestId("high-score-display");
+    rerender(<Scoreboard score={1}/>)
 
-test("shows a high score component", () => {
+    const currentScore = screen.getByTestId("current-score-display");
+
+    expect(currentScore.textContent).toBe("1");
+})
+
+test("shows a high score component, which starts as 0", () => {
+    render(<Scoreboard />);
+
+    const highScore = screen.getByTestId("high-score-display");
     expect(highScore).toBeDefined();
+    expect(highScore.textContent).toBe("0")
 });
 
-test("starts as 0", () => {
-    expect(highScore.textContent).toBe("0")
+test("updates when the score is more than the high score", () => {
+    const { rerender } = render(<Scoreboard score={0}/>);
+    rerender(<Scoreboard score={5}/>);
+
+    const highScore = screen.getByTestId("high-score-display");
+
+    expect(highScore.textContent).toBe("5")
+})
+
+test("does not update when the score is less than the high score", () => {
+    const { rerender } = render(<Scoreboard score={0}/>);
+    rerender(<Scoreboard score={5}/>);
+    const highScore = screen.getByTestId("high-score-display");
+    expect(highScore).toHaveTextContent("5");
+
+    rerender(<Scoreboard score={0}/>);
+    expect(highScore.textContent).toBe("5")
 })
