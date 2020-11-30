f450835a94f782d335ced038c09873d2c5681744
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2020-11-30 17:04:15 -0300

    feat: 🏆 Add high score support (#169)
    * Add high score support to the scoreboard
    
    * Manage high score in state instead of props
    
    * Update high score when score updates
    
    * Persist high score
    
    * Style updated scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,8 +2,32 @@ import React from "react";
 import "./Scoreboard.css";
 
 class Scoreboard extends React.Component {
+  state = {
+    highScore: 0
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem("highScore") > 0) {
+      this.setState({ highScore: localStorage.getItem("highScore")})
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.score > this.state.highScore) {
+      localStorage.setItem("highScore", this.props.score);
+      this.setState({ highScore: this.props.score })
+    }
+  }
+
   render() {
-    return <div className="scoreboard">Score: {this.props.score}</div>;
+    return <div className="scoreboard">
+      <p className="current-score">
+      Score: <span data-testid="current-score-display">{this.props.score}</span>
+      </p>
+      <p className="high-score">
+        High Score: <span data-testid="high-score-display">{this.state.highScore}</span>
+      </p>
+      </div>;
   }
 }
 
