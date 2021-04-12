ce39b2ba52f04b32e53adf412655f14dab1d2cf3
Ian MacIntosh <ian@ianjmacintosh.com>
2021-04-12 17:31:39 -0300

    Send players to /play

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "./App";
+import Board from "./Board";
 import FileNotFound from "./FileNotFound";
 
 const Router = () => {
@@ -9,7 +10,7 @@ const Router = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={App}></Route>
-        <Route exact path="/play" component={App}></Route>
+        <Route exact path="/play" component={Board}></Route>
         <Route exact path="/credits" component={App}></Route>
         <Route exact path="/instructions" component={App}></Route>
         <Route component={FileNotFound}></Route>
