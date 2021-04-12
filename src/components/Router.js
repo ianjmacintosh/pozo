be18c2f581e8e032697b51bc377e51820150a2a3
Ian MacIntosh <ian@ianjmacintosh.com>
2021-04-12 15:44:04 -0300

    Add router and filenotfound component

---
+++ src/components/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import App from "./App";
+import FileNotFound from "./FileNotFound";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App}></Route>
+        <Route exact path="/play" component={App}></Route>
+        <Route exact path="/credits" component={App}></Route>
+        <Route exact path="/instructions" component={App}></Route>
+        <Route component={FileNotFound}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
